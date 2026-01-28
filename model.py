9fffac76b3ddee7cca9f222726483e45f587c989
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-01-28 13:23:53 -0500

    Replaced deprecated use of 'pretrained' with default 'weights'.

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import torch
-from torchvision.models.detection import maskrcnn_resnet50_fpn
+from torchvision.models.detection import maskrcnn_resnet50_fpn, MaskRCNN_ResNet50_FPN_Weights
 
 def load_model():
     # Currently loads a pretrained Mask R-CNN model (Will train my own later)
 
-    model = maskrcnn_resnet50_fpn(pretrained=True)
+    weights = MaskRCNN_ResNet50_FPN_Weights.DEFAULT
+    model = maskrcnn_resnet50_fpn(pweights=weights)
     model.eval() #Inference mode
     return model
