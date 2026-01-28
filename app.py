2ae67afca6aa0434e159fa3229eb46a03a9d582a
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-01-28 12:25:43 -0500

    App.py is semifunctional. It returns the number of images found in the folder and the tensor of one image.

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
+
+from utils import get_image_paths
+from inference import load_image
+
 
 def select_folder():
     folder = filedialog.askdirectory()
-    if folder:
-        print("Selected folder:", folder)
+    if not folder:
+        return
+    
+    image_paths = get_image_paths(folder)
+
+    if len(image_paths) == 0:
+        messagebox.showwarning("No Images", "No images found in selected folder.")
+        return
+    
+    first_image = load_image(image_paths[0])
+
+    messagebox.showinfo(
+        "Images Loaded\n"
+        f"found {len(image_paths)} images.\n"
+        f"First image tensor shape:\n[first_image.shape]"
+    )
 
 root = tk.Tk()
 root.title("Herd Counter")
--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ def load_image(image_path):
     image_array = np.array(image)
 
     # Divide by MAX_RGB to normalize between 0 and 1
-    image_tensor = torch.from_nupy(image_array).float() / MAX_RGB
+    image_tensor = torch.from_numpy(image_array).float() / MAX_RGB
 
     # Change from CHW to HWC (Default for Pytorch but not Pillow)
     image_tensor = image_tensor.permute(2,0,1)
